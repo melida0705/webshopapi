3ac35a67717d3d4dcec7061ca5e2ffaf154a034c
Melida Radoncic <melidaradoncic@hotmail.com>
2020-09-01 03:21:43 +0200

    update messages

--- MessageContact.php
+++ MessageContact.php
@@ -14,26 +14,35 @@ if(!$customer_id)
 $messages=[];
 
 // Delete.
-$proba2="SELECT DISTINCT receiver_id FROM `messages` WHERE sender_id='{$customer_id}'";
+$proba2="SELECT DISTINCT receiver_id,sender_id FROM `messages` WHERE (sender_id='{$customer_id}' or receiver_id='{$customer_id}')";
 
    
   if($result1=mysqli_query($con,$proba2))
      {
-     
+         
             $cr = 0;
             while($row = mysqli_fetch_assoc($result1))
             { 
-               
-                $messages[$cr]['receiver_id'] = $row['receiver_id'];
-                
+                if($customer_id==$row['receiver_id'] ){
+                    $messages[$cr]['receiver_id'] = $row['sender_id'];
+                    $cr++;
+                  }
+               if($customer_id==$row['sender_id'])
+                  {
+                    $messages[$cr]['receiver_id']=$row['receiver_id'];
+                    $cr++;
+                  }
+                  
+               // $messages[$cr]['sender_id']=$row['receiver_id'];
+             //  }
               
              
                 
-                $cr++;
+                
             }
             
                        echo json_encode(['data'=>$messages]);
     }
-    
+    ?>
                   
-  ?>
+  
