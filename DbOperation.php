2e8fcefeec9d2364424f72bd2c7321c6cd8eb5d4
melida0705 <44578817+melida0705@users.noreply.github.com>
2020-10-04 23:22:16 +0200

    Update DbOperation.php

--- DbOperation.php
+++ DbOperation.php
@@ -20,7 +20,7 @@ class DbOperation
 	}
 	
 	function getSlike(){
-		$stmt = $this->con->prepare("SELECT * FROM images");
+		$stmt = $this->con->prepare("SELECT id,url,opis,lajkovi FROM images");
 		$stmt->execute();
 		$stmt->bind_result($id, $url, $opis, $lajkovi);
 		
