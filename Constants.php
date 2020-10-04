be6f5b4e6b3e24d01cde15d4b482d2d349a5181f
Melida Radoncic <melidaradoncic@hotmail.com>
2020-10-04 23:13:00 +0200

    s

---
+++ Constants.php
@@ -0,0 +1,10 @@
+<?php 
+
+	/*
+	* Created by Belal Khan
+	* website: www.simplifiedcoding.net 
+	*/
+	define('DB_HOST', 'sql2.freemysqlhosting.net');
+	define('DB_USER', 'sql2368143');
+	define('DB_PASS', 'xF2%yL3*');
+	define('DB_NAME', 'sql2368143');
