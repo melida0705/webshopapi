450b786a3f5fb7ac67226e1ef877279cfc832fa2
melida0705 <44578817+melida0705@users.noreply.github.com>
2020-10-04 22:17:35 +0200

    Update connect.php

--- connect.php
+++ connect.php
@@ -1,10 +1,10 @@
 <?php
 
 // db credentials
-define('DB_HOST', 'djbinno.com');
-define('DB_USER', 'condosharp');
-define('DB_PASS', 'Miamilakesdj5881');
-define('DB_NAME', 'webshop');
+define('DB_HOST', 'sql2.freemysqlhosting.net');
+define('DB_USER', 'sql2368143');
+define('DB_PASS', 'xF2%yL3*');
+define('DB_NAME', 'sql2368143');
 
 // Connect with the database.
 function connect()
