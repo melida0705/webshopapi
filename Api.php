9fad6ebf97380031dbc8280307251e1ef0ad1a98
melida0705 <44578817+melida0705@users.noreply.github.com>
2020-10-05 10:32:00 +0200

    Update Api.php

--- Api.php
+++ Api.php
@@ -68,7 +68,24 @@
 					$response['message'] = 'Nothing to delete, provide an id please';
 				}
 			break; 
+			case 'user':
 
+				//for the delete operation we are getting a GET parameter from the url having the id of the record to be deleted
+				if(isset($_GET['id'])){
+					$db = new DbOperation();
+					if($db->login($_GET['username'], $_GET['password'])){
+						$response['error'] = false; 
+						$response['message'] = 'Like updated successfully';
+						$response['user']=getUser();
+					}else{
+						$response['error'] = true; 
+						$response['message'] = 'Some error occurred please try again';
+					}
+				}else{
+					$response['error'] = true; 
+					$response['message'] = 'Nothing to delete, provide an id please';
+				}
+			break; 
 			case 'createhero':
 				//first check the parameters required for this request are available or not 
 				isTheseParametersAvailable(array('name','realname','rating','teamaffiliation'));
@@ -105,7 +122,41 @@
 				}
 				
 			break; 
-			
+			case 'createimage':
+				//first check the parameters required for this request are available or not 
+				isTheseParametersAvailable(array('opis','lajkovi'));
+				
+				//creating a new dboperation object
+				$db = new DbOperation();
+				
+				//creating a new record in the database
+				$result = $db->createImage(
+					$_POST['opis'],
+					$_POST['lajkovi'],
+					
+				);
+				
+
+				//if the record is created adding success to response
+				if($result){
+					//record is created means there is no error
+					$response['error'] = false; 
+
+					//in message we have a success message
+					$response['message'] = 'Hero addedd successfully';
+
+					//and we are getting all the heroes from the database in the response
+					$response['heroes'] = $db->getSlike();
+				}else{
+
+					//if record is not added that means there is an error 
+					$response['error'] = true; 
+
+					//and we have the error message
+					$response['message'] = 'Some error occurred please try again';
+				}
+				
+			break; 
 			//the READ operation
 			//if the call is getheroes
 			case 'getheroes':
