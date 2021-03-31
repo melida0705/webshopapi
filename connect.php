7290d70d6482b63b716d86d0937e7b82f57a5f61
melida0705 <44578817+melida0705@users.noreply.github.com>
2021-03-31 21:18:46 +0200

    Update connect.php

--- connect.php
+++ connect.php
@@ -9,7 +9,7 @@ define('DB_NAME', 'webshop');
 // Connect with the database.
 function connect()
 {
-  $connect = mysqli_connect(DB_HOST ,DB_USER ,DB_PASS ,DB_NAME);
+  $connect = mysqli(DB_HOST ,DB_USER ,DB_PASS ,DB_NAME);
 
   if (mysqli_connect_errno($connect)) {
     die("Failed to connect:" . mysqli_connect_error());
